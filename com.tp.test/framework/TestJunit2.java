99a665b0c9288decae0cf7535c5e5201fce1169a
Marlon <martianworm17@gmail.com>
2017-12-11 21:33:26 +0800

    JUnit - Test Framework - Test Suites

---
+++ com.tp.test/framework/TestJunit2.java
@@ -0,0 +1,20 @@
+package framework;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+
+public class TestJunit2 {
+	
+	String message = "Robert";
+	MessageUtil messageUtil = new MessageUtil(message);
+	
+	@Test
+	public void testSalutationMessage() {
+		System.out.println("Inside testSalutationMessage()");
+		message = "Hi!" + "Robert";
+		assertEquals(message, messageUtil.salutationMessage());
+	}
+	
+	
+}
