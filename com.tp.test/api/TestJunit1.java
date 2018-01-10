118e50d3a6260a6737706d32a35be838c8293aa0
Marlon <martianworm17@gmail.com>
2018-01-10 21:24:21 +0800

    JUnit - API - Assert Class

---
+++ com.tp.test/api/TestJunit1.java
@@ -0,0 +1,31 @@
+package api;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestJunit1 {
+
+	@Test
+	public void testAdd() {
+		
+		// Test data
+		int num = 5;
+		String temp = null;
+		String str = "Junit is working fine";
+		
+		// Check for equality
+		assertEquals("Junit is working fine", str);
+		
+		// Check for false condition
+		assertFalse(num > 6);
+		
+		// Check for not null value
+		assertNotNull(str);
+		
+		// Check for null value
+		assertNull(temp);
+		
+		// Check for not null value
+		//assertNotNull(temp);
+	}
+}
