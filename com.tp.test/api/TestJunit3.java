9857e0a0fa2389143942169753a64caaf7652694
Marlon <martianworm17@gmail.com>
2018-01-11 20:29:53 +0800

    JUnit - API - TestResult Class

---
+++ com.tp.test/api/TestJunit3.java
@@ -0,0 +1,29 @@
+package api;
+
+import org.junit.Test;
+import junit.framework.AssertionFailedError;
+import junit.framework.TestResult;
+
+public class TestJunit3 extends TestResult {
+	
+	// Add the error
+	public synchronized void addError(Test test, Throwable t) {
+		super.addError((junit.framework.Test) test, t);
+	}
+	
+	// Add the failure
+	public synchronized void addFailure(Test test, AssertionFailedError t) {
+		super.addFailure((junit.framework.Test) test, t);
+	}
+	
+	@Test
+	public void testAdd() {
+		// Add any test
+	}
+	
+	// Marks that the test run should stop.
+	public synchronized void stop() {
+		// Stop the test here
+	}
+
+}
