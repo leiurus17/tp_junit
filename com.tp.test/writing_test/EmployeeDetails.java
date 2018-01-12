50cd78d353bd230cb7f3f4d394f8156982955333
Marlon <martianworm17@gmail.com>
2018-01-12 18:56:29 +0800

    JUnit - Writing a Test - EmployeeDetails.java

---
+++ com.tp.test/writing_test/EmployeeDetails.java
@@ -0,0 +1,48 @@
+package writing_test;
+
+public class EmployeeDetails {
+
+	private String name;
+	private double monthlySalary;
+	private int age;
+	
+	/**
+	 * @return the name
+	 */
+	
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * @param name the name to set
+	 */
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * @return the monthlySalary
+	 */
+	
+	public void setMonthlySalary(double monthlySalary) {
+		this.monthlySalary = monthlySalary;
+	}
+	
+	/**
+	 * @return the age
+	 */
+	
+	public int getAge() {
+		return age;
+	}
+	
+	/**
+	 * @param age the age to set
+	 */
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
