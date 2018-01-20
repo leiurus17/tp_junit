1ba17a3a34660ee577946eb650cb503716240ee0
Marlon <martianworm17@gmail.com>
2018-01-20 16:18:29 +0800

    JUnit - Extensions - MockObject NOT WORKING

---
+++ com.tp.test/mockobject/PubTest.java
@@ -0,0 +1,37 @@
+package mockobject;
+
+import org.jmock.Mockery;
+
+import junit.framework.TestCase;
+
+public class PubTest {
+	
+//	class Mock {
+//		Mockery context = new Mockery();
+//		
+//		public void testSubReceivesMessage() {
+//			
+//			// Set up
+//			final Sub sub = context.mock(Sub.class);
+//			
+//			Pub pub = new Pub();
+//			pub.add(sub);
+//			
+//			final String message = "message";
+//			
+//			// expectations
+//			context.checking(new Expectations() {
+//				oneOf (sub).receive(message);
+//			});
+//			
+//			// execute
+//			pub.publish(message);
+//			
+//			// verify
+//			context.assertIsSatisfied();
+//			
+//			
+//		}
+//	}
+
+}
